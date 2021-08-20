9a83bec2f4cfbcc4cde8a5a60aa98cae35e737cb
Paige Tiedeman <paigetiedeman@gmail.com>
2021-08-20 13:00:16 -0700

    numberArray functioning properly!

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,14 @@ function rogerReplace(number){
   const three = "Won't you be my neighbor?"
   const two = "Boop!"
   const one = "Beep!"
-  let numberString = number.toString();
   const numberArray = [];
-  for (let i = 0; i < numberArray.length; i++) {
-    if (numberString[i].includes(3)) {
+  for (let i = 0; i <= number; i++) {
+  let number = i.toString();   
+    if (number.includes(3)) {
       numberArray.push(three);
-    } else if (i == 2 && !numberString[i].includes(3)) {
+    } else if (number.includes(2)) {
       numberArray.push(two);
-    } else if (i == 1 && !numberString[i].includes(3) && !numberString[i].includes(2)) {
+    } else if (number.includes(1)) {
       numberArray.push(one);
     } else {
       numberArray.push(i)
