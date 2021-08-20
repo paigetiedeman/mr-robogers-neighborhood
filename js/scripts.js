18961c21d4a1f81d2bc09104eac56d3ee5de4ab5
Paige Tiedeman <paigetiedeman@gmail.com>
2021-08-20 12:51:42 -0700

    WIP: still working on returning  the array

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,14 @@ function rogerReplace(number){
   const three = "Won't you be my neighbor?"
   const two = "Boop!"
   const one = "Beep!"
+  let numberString = number.toString();
   const numberArray = [];
-  for (let i = 0; i <= number; i++) {
-    if (number.toString().includes(3)) {
+  for (let i = 0; i < numberArray.length; i++) {
+    if (numberString[i].includes(3)) {
       numberArray.push(three);
-    } else if (number.toString().includes(2)) {
+    } else if (i == 2 && !numberString[i].includes(3)) {
       numberArray.push(two);
-    } else if (number.toString().includes(1)) {
+    } else if (i == 1 && !numberString[i].includes(3) && !numberString[i].includes(2)) {
       numberArray.push(one);
     } else {
       numberArray.push(i)
